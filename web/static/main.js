e567349340f2f3c450e9f101efffa20477e56cde
VladWithCode <vladwithcode@gmail.com>
2024-01-26 17:03:27 -0600

    Static & Templates

---
+++ web/static/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("htmx:responseError", e => {
+        const message = e.detail.xhr.responseText;
+
+        alert(message || "Ocurrio un error al recuperar el expediente solicitado.")
+    })
+})
