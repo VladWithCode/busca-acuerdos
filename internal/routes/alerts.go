bac84d76c3172b9bc2f9a379a06b6cd920e99505
VladWithCode <vladwithcode@gmail.com>
2024-03-08 12:21:43 -0600

    Add Create Alert route
    Also moves sendTestMessage to Reports

--- internal/routes/alerts.go
+++ internal/routes/alerts.go
@@ -3,74 +3,36 @@ package routes
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/julienschmidt/httprouter"
-	"github.com/vladwithcode/juzgados/internal/whatsapp"
+	"github.com/vladwithcode/juzgados/internal/auth"
+	"github.com/vladwithcode/juzgados/internal/db"
 )
 
-func SendTestMessage(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	var (
-		headerVars []whatsapp.TemplateVar
-		bodyVars   []whatsapp.TemplateVar
-	)
-
-	y, m, d := time.Now().Date()
-
-	var (
-		dStr string
-		mStr string
-	)
-
-	if d < 10 {
-		dStr = fmt.Sprintf("0%d", d)
-	} else {
-		dStr = fmt.Sprintf("%d", d)
-	}
-
-	if m < 10 {
-		mStr = fmt.Sprintf("0%d", m)
-	} else {
-		mStr = fmt.Sprintf("%d", m)
-	}
-
-	headerVars = append(headerVars, whatsapp.TemplateVar{
-		"type": "document",
-		"document": struct {
-			Link     string `json:"link"`
-			Filename string `json:"filename"`
-		}{Link: "https://www.postgresql.org/files/documentation/pdf/16/postgresql-16-US.pdf", Filename: "reporte-.pdf"},
-	})
-
-	bodyVars = append(bodyVars, whatsapp.TemplateVar{
-		"type": "text",
-		"text": "Jairo Rangel",
-	})
-	bodyVars = append(bodyVars, whatsapp.TemplateVar{
-		"type": "date_time",
-		"date_time": struct {
-			FallbackValue string `json:"fallback_value"`
-		}{
-			FallbackValue: fmt.Sprintf("%v-%v-%v", y, mStr, dStr),
-		},
-	})
-
-	err := whatsapp.SendTemplateMessage("+526183188452", whatsapp.TemplateData{
-		TemplateName: "report_file",
-		HeaderVars:   headerVars,
-		BodyVars:     bodyVars,
-	})
-
-	w.Header().Add("Content-Type", "text/html")
+func CreateAlert(w http.ResponseWriter, r *http.Request, _ httprouter.Params, auth *auth.Auth) {
+	err := r.ParseForm()
 
 	if err != nil {
-		fmt.Printf("err: %v\n", err)
-		w.WriteHeader(500)
-		w.Write([]byte("<p>Error</p>"))
-
+		fmt.Printf("[Parse Form Err]: %v\n", err)
+		respondWithError(w, 400, "La información proporcionada no es válida")
 		return
 	}
 
-	w.WriteHeader(200)
-	w.Write([]byte("<p>Success</p>"))
+	fmt.Printf("r.Form: %+v\n", r.Form)
+
+	var (
+		caseId     string = r.Form.Get("caseId")
+		natureCode string = r.Form.Get("natureCode")
+		userId     string = auth.Id
+	)
+
+	alert, err := db.CreateAlert(userId, caseId, natureCode)
+
+	if err != nil {
+		fmt.Printf("[Create Err]: %v\n", err)
+		respondWithError(w, 500, "Ocurrió un error al crear la alerta")
+		return
+	}
+
+	respondWithJSON(w, 201, alert)
 }
