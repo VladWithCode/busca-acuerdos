28d02030ef5c82a0d0f2dd58782fe96c031d27ac
VladWithCode <vladwithcode@gmail.com>
2024-03-08 12:25:42 -0600

    Add Report route handlers

---
+++ internal/routes/reports.go
@@ -0,0 +1,100 @@
+package routes
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/vladwithcode/juzgados/internal/whatsapp"
+)
+
+func ReportHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	cases := r.URL.Query()["cases"]
+
+	templ, err := template.ParseFiles("web/templates/reports/layout.html", "web/templates/reports/alert-report.html")
+
+	fmt.Printf("cases: %v\n", cases)
+
+	if err != nil {
+		fmt.Println(err)
+		respondWithError(w, 500, "Server Error")
+	}
+
+	templ.Execute(w, struct {
+		ReportTitle string
+	}{
+		ReportTitle: "",
+	})
+}
+
+func CreateReportForCase(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	//data :=
+}
+
+func SendTestMessage(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	var (
+		headerVars []whatsapp.TemplateVar
+		bodyVars   []whatsapp.TemplateVar
+	)
+
+	y, m, d := time.Now().Date()
+
+	var (
+		dStr string
+		mStr string
+	)
+
+	if d < 10 {
+		dStr = fmt.Sprintf("0%d", d)
+	} else {
+		dStr = fmt.Sprintf("%d", d)
+	}
+
+	if m < 10 {
+		mStr = fmt.Sprintf("0%d", m)
+	} else {
+		mStr = fmt.Sprintf("%d", m)
+	}
+
+	headerVars = append(headerVars, whatsapp.TemplateVar{
+		"type": "document",
+		"document": struct {
+			Link     string `json:"link"`
+			Filename string `json:"filename"`
+		}{Link: "https://www.postgresql.org/files/documentation/pdf/16/postgresql-16-US.pdf", Filename: "reporte-.pdf"},
+	})
+
+	bodyVars = append(bodyVars, whatsapp.TemplateVar{
+		"type": "text",
+		"text": "Jairo Rangel",
+	})
+	bodyVars = append(bodyVars, whatsapp.TemplateVar{
+		"type": "date_time",
+		"date_time": struct {
+			FallbackValue string `json:"fallback_value"`
+		}{
+			FallbackValue: fmt.Sprintf("%v-%v-%v", y, mStr, dStr),
+		},
+	})
+
+	err := whatsapp.SendTemplateMessage("+526183188452", whatsapp.TemplateData{
+		TemplateName: "report_file",
+		HeaderVars:   headerVars,
+		BodyVars:     bodyVars,
+	})
+
+	w.Header().Add("Content-Type", "text/html")
+
+	if err != nil {
+		fmt.Printf("err: %v\n", err)
+		w.WriteHeader(500)
+		w.Write([]byte("<p>Error</p>"))
+
+		return
+	}
+
+	w.WriteHeader(200)
+	w.Write([]byte("<p>Success</p>"))
+}
