eb41566476f8a551ba15acda739d2df738406add
VladWithCode <vladwithcode@gmail.com>
2024-03-22 02:27:24 -0600

    Change Listen for htmx events and handle errors
    Define custom handling for status codes that are treated as errors (>=
    400)

--- web/static/main.js
+++ web/static/main.js
@@ -1,8 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.body.addEventListener("htmx:responseError", e => {
-        const message = e.detail.xhr.responseText;
+    document.body.addEventListener("htmx:afterRequest", () => {
+        const finishLoadEvt = CustomEvent("custom:", { detail: { keepModalOpen: true } }) 
+        document.body.dispatchEvent(finishLoadEvt)
+    })
+    document.body.addEventListener("htmx:beforeSwap", e => {
+        let status = e.detail.xhr.status
 
-        alert(message || "Ocurrio un error al recuperar el expediente solicitado.")
+        if (status < 400) {
+            return
+        }
+
+        if (status >= 400 || status < 500) {
+            e.detail.target = htmx.find("#modal-wrapper")
+            e.detail.shouldSwap = true
+            return
+        }
+
+        if (status >= 500) {
+            if (typeof window.createErrorModal === 'function') {
+                document
+                    .querySelector()
+                    .insertAdjacentHTML(
+                        "beforeend",
+                        window.createErrorModal({ message: "Ocurrio un error inesperado", btnLabel: "Aceptar" })
+                    )
+
+                return
+            }
+
+            alert("Ocurrió un error inesperado")
+        }
     })
 })
 
