7b9bdfde8f03098ff8a4cc9f5a5e588bc7958f73
vladwithcode <vladwithcode@gmail.com>
2024-03-25 10:46:04 +0000

    Setup for prod

--- cmd/auto-report/auto-report.go
+++ cmd/auto-report/auto-report.go
@@ -25,45 +25,64 @@ type subscriberMeta struct {
 type subscriberMap map[string][]subscriberMeta
 
 func main() {
-	homePath, _ := os.UserHomeDir()
+	homePath, err := os.UserHomeDir()
 
-	if homePath == "" {
-		homePath = "/home/vladwb/"
+	if err != nil {
+		log.Println("Error: Couldn't load UserHomeDir")
+		os.Exit(1)
+	}
+
+	if err != nil {
+		fmt.Printf("Open Log err: %v\n", err)
+		os.Exit(1)
 	}
 
 	stdOut, err := os.OpenFile(homePath+"/.local/log/auto-report.log/log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		fmt.Printf("Open Log err: %v\n", err)
+		os.Exit(1)
 	}
+
 	stdErr, err := os.OpenFile(homePath+"/.local/log/auto-report.log/error", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		fmt.Printf("Open Err err: %v\n", err)
+		os.Exit(1)
 	}
 
 	if stdOut != nil && stdErr != nil {
 		os.Stdout = stdOut
 		os.Stderr = stdErr
 	}
-	err = godotenv.Load("/home/vladwb/me/Dev/go/juzgados/.env")
+
+	tsjDir := os.Getenv("TSJ_DIR")
+
+	err = godotenv.Load(fmt.Sprintf("%v/.env", tsjDir))
+
 
 	if err != nil {
-		log.Fatalf("env err: %v\n", err)
-		return
+		log.Printf("Error: Couldn't load enviroment %v\n", err)
+		os.Exit(1)
+	}
+	hostname := os.Getenv("TSJ_SITE_HOSTNAME")
+
+	if hostname == "" {
+		log.Println("Error: env hostname is missing")
+		os.Exit(1)
 	}
 
 	dbPool, err := db.Connect()
 
 	if err != nil {
-		log.Fatalf("Error while connecting to DB: %v", err)
-		return
+		log.Printf("Error while connecting to DB: %v", err)
+		os.Exit(1)
 	}
 	defer dbPool.Close()
 
 	userAlerts, err := db.FindAutoReportAlertsWithUserData()
 
 	if err != nil {
-		log.Fatalf("Find alerts err: %v\n", err)
-		return
+		log.Printf("Find alerts err: %v\n", err)
+		os.Exit(1)
 	}
 
 	subscribers := subscriberMap{}
@@ -71,7 +90,6 @@ func main() {
 
 	for _, user := range userAlerts {
 		for alertIdx, alert := range user.Alerts {
-			//cK := fmt.Sprintf("%v-%v", alert.CaseId, alert.NatureCode)
 			cK := alert.GetCaseKey()
 
 			suscriber := subscriberMeta{
@@ -94,8 +112,8 @@ func main() {
 	resCases, err := tsj.GetCasesData(caseKeys, 30)
 
 	if err != nil {
-		log.Fatalf("GetCases err: %v\n", err)
-		return
+		log.Printf("GetCases err: %v\n", err)
+		os.Exit(1)
 	}
 
 	for _, c := range resCases.Docs {
@@ -122,14 +140,15 @@ func main() {
 			_, err := alerts.GenReportPdfWithData(*user)
 
 			if err != nil {
-				log.Fatalf("GenReport err: %v\n", err)
+				log.Printf("GenReport err: %v\n", err)
 				return
 			}
 
-			err = whatsapp.SendReportMessage(*user, "http://tsjdgo.gob.mx/Recursos/images/flash/ListasAcuerdos/1132024/fam2.pdf")
+			docHref := fmt.Sprintf("http://%v/reports/%v/report.pdf", hostname, user.Id)
+			err = whatsapp.SendReportMessage(*user, docHref)
 
 			if err != nil {
-				log.Fatalf("SendReport err: %v\n", err)
+				log.Printf("SendReport err: %v\n", err)
 				return
 			}
 		}(user)
--- scripts/auto-report.sh
+++ scripts/auto-report.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-cd /home/vladwb/me/Dev/go/juzgados
-go build -o cmd/auto-report/auto-report cmd/auto-report/auto-report.go 2>&1>/dev/null || awk '{print "Build Err: "$0}' | tee -a ~/.local/log/auto-report.log/error
-cmd/auto-report/auto-report 2>&1>/dev/null || awk '{print "Exec Err: "$0}' | tee -a ~/.local/log/auto-report.log/error
+tsjDir=$TSJ_DIR
+cd $tsjDir
+
+go build -o cmd/auto-report/auto-report cmd/auto-report/auto-report.go
+cmd/auto-report/auto-report
